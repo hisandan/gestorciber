34782c47af97bf2a0fd5144eccdfdd92af112baf
Daniel Santiago Sandoval Higuera <95585487+hisandan@users.noreply.github.com>
2025-06-19 03:48:43 +0000

    testing new deploy using branch gh-pages

--- vite-dev/src/pages/Home.tsx
+++ vite-dev/src/pages/Home.tsx
@@ -15,7 +15,7 @@ function Home() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Gestor Ciber</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
